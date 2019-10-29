8b3a262784541cb49c2343c7cc118cfface02c43
soarflat <soarflat777@gmail.com>
2019-10-29 17:18:50 +0900

    feat: 必要なコンポーネントファイルをとりあえず追加 (#1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useReducer, createContext } from 'react';
+import Login from './components/Login';
+import Home from './components/Home';
+import Header from './components/Header';
+
+export const AuthContext = createContext();
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      localStorage.setItem('user', JSON.stringify(action.payload.user));
+      localStorage.setItem('token', JSON.stringify(action.payload.token));
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+        token: action.payload.token
+      };
+    case 'LOGOUT':
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <AuthContext.Provider value={{ state, dispatch }}>
+      <Header />
+      <div>{state.isAuthenticated ? <Home /> : <Login />}</div>
+    </AuthContext.Provider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <nav id="navigation">
+      <h1 href="#" className="logo">
+        HOOKED
+      </h1>
+    </nav>
+  );
+};
+
+export default Header;
