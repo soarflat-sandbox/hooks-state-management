2391cf9e5a7891a837a155359a92265c4d35d6c0
soarflat <soarflat777@gmail.com>
2019-10-31 19:36:07 +0900

    refactor: reducer を別ファイルに分割 (#1)

--- src/App.js
+++ src/App.js
@@ -2,35 +2,9 @@ import React, { useReducer, createContext } from 'react';
 import Login from './components/Login';
 import Home from './components/Home';
 import Header from './components/Header';
+import reducer, { initialState } from './reducer';
 
 export const AuthContext = createContext();
-const initialState = {
-  isAuthenticated: false,
-  user: null,
-  token: null
-};
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'LOGIN':
-      localStorage.setItem('user', JSON.stringify(action.payload.user));
-      localStorage.setItem('token', JSON.stringify(action.payload.token));
-      return {
-        ...state,
-        isAuthenticated: true,
-        user: action.payload.user,
-        token: action.payload.token
-      };
-    case 'LOGOUT':
-      localStorage.clear();
-      return {
-        ...state,
-        isAuthenticated: false,
-        user: null
-      };
-    default:
-      return state;
-  }
-};
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
